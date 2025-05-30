7a1f0c0bb218fa886cd0a2768549875f3f77be05
balamuru <balamuru@gmail.com>
2025-05-30 18:48:58 -0500

    Initial commit

---
+++ src/main/java/ccom/vgb/k8s_sample_deploy/K8sSampleDeployApplication.java
@@ -0,0 +1,21 @@
+package ccom.vgb.k8s_sample_deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class K8sSampleDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K8sSampleDeployApplication.class, args);
+	}
+
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello, Kubernetes!!!!!";
+	}
+}
---
+++ src/test/java/ccom/vgb/k8s_sample_deploy/K8sSampleDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package ccom.vgb.k8s_sample_deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8sSampleDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
